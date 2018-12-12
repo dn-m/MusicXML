80c69b2f1715d4abcc95cbd2c1d1c03ce245aa39
jsbean <jbean46@gmail.com>
2018-12-12 07:12:11 -0500

    Move [type] subscript and [duration] subscript up a level

--- Sources/MusicXML/Deserializer/Elements/NoteDeserializer.swift
+++ Sources/MusicXML/Deserializer/Elements/NoteDeserializer.swift
@@ -12,27 +12,27 @@ extension MusicXML {
     static func deserializeNote(_ noteIndexer: XMLIndexer) throws -> Note {
         return Note(
             pitch: try deserializePitch(noteIndexer["pitch"]),
-            duration: try deserializeDuration(noteIndexer),
-            durationType: try deserializeDurationType(noteIndexer)
+            duration: try deserializeDuration(noteIndexer["duration"]),
+            durationType: try deserializeDurationType(noteIndexer["type"])
         )
     }
 
-    static func deserializePitch(_ noteIndexer: XMLIndexer) throws -> Pitch {
+    static func deserializePitch(_ pitchIndexer: XMLIndexer) throws -> Pitch {
         return Pitch(
-            step: try noteIndexer["step"].value(),
-            octave: try noteIndexer["octave"].value()
+            step: try pitchIndexer["step"].value(),
+            octave: try pitchIndexer["octave"].value()
         )
     }
 
-    static func deserializeDuration(_ noteIndexer: XMLIndexer) throws -> Int {
-        return try noteIndexer["duration"].value()
+    static func deserializeDuration(_ durationIndexer: XMLIndexer) throws -> Int {
+        return try durationIndexer.value()
     }
 
-    static func deserializeDurationType(_ noteIndexer: XMLIndexer) throws -> DurationType {
-        guard let kind = DurationType.Kind(rawValue: try noteIndexer["type"].value()) else {
-            throw Error.invalidDurationType(noteIndexer)
+    static func deserializeDurationType(_ typeIndexer: XMLIndexer) throws -> DurationType {
+        guard let kind = DurationType.Kind(rawValue: try typeIndexer.value()) else {
+            throw Error.invalidDurationType(typeIndexer)
         }
-        let size = (try? noteIndexer["type"].value(ofAttribute: "size"))
+        let size = (try? typeIndexer.value(ofAttribute: "size"))
             .flatMap { DurationType.Size(rawValue: $0) }
         return DurationType(kind: kind, size: size ?? .full)
     }
