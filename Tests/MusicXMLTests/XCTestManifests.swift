df9dfcf4192329af8b3065ae4822417ad09ef680
James <jbean46@gmail.com>
2018-12-16 21:08:59 -0500

    Remove old HelloWorldTests

--- Tests/MusicXMLTests/XCTestManifests.swift
+++ Tests/MusicXMLTests/XCTestManifests.swift
@@ -1,15 +1,23 @@
 import XCTest
 
-extension HelloWorldTests {
+extension DecoderTests {
     static let __allTests = [
+        ("testClef", testClef),
         ("testHelloWorld", testHelloWorld),
+        ("testKey", testKey),
+        ("testMeasure", testMeasure),
+        ("testNote", testNote),
+        ("testPart", testPart),
+        ("testPartList", testPartList),
+        ("testPitch", testPitch),
+        ("testTime", testTime),
     ]
 }
 
 #if !os(macOS)
 public func __allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(HelloWorldTests.__allTests),
+        testCase(DecoderTests.__allTests),
     ]
 }
 #endif
