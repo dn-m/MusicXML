c0f37c99ebf12dc2abcea73d2145d877a6e60711
James <jbean46@gmail.com>
2018-12-18 16:14:14 -0800

    Remove dead code

--- Sources/MusicXML/Model/Elements/Note.swift
+++ Sources/MusicXML/Model/Elements/Note.swift
@@ -111,29 +111,10 @@ extension MusicXML {
             case accidental
         }
 
-        // instrument?, %editorial-voice;, type?, dot*,
-        //  accidental?, time-modification?, stem?, notehead?,
-        //  notehead-text?, staff?, beam*, notations*, lyric*, play?)>
-
         let pitch: Pitch
         let duration: Int // amount of "divisions"
         let durationType: DurationType
         let accidental: Accidental?
-//
-//        public init(from decoder: Decoder) throws {
-//            print("attempt to decode note")
-//            var container = try decoder.unkeyedContainer()
-//            var nested = try container.nestedContainer(keyedBy: CodingKeys.self)
-//
-//            print("container is good")
-//            self.pitch = try nested.decode(Pitch.self, forKey: .pitch)
-//            print("p is good")
-//            self.duration = try nested.decode(Int.self, forKey: .duration)
-//            print("d is good")
-//            self.durationType = try nested.decode(DurationType.self, forKey: .durationType)
-//            print("we made it this far... to d type")
-//            self.accidental = nil
-//        }
     }
 
     // > Pitch is represented as a combination of the step of the
