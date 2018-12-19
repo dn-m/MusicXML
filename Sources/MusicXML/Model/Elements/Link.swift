05f4de0d8f4e85c2f6d9d54c0274751f57ebd79e
James Bean <jbean46@gmail.com>
2018-12-18 21:37:29 -0800

    Build out Credit and friends (#15)

--- Sources/MusicXML/Model/Elements/Link.swift
+++ Sources/MusicXML/Model/Elements/Link.swift
@@ -5,25 +5,20 @@
 //  Created by James Bean on 12/3/18.
 //
 
-// MARK: TODO
+// MusicXML link.mod module
+// Version 3.1
 //
-//<!--
-//    MusicXML link.mod module
-//    Version 3.1
+// Copyright © 2004-2017 the Contributors to the MusicXML
+// Specification, published by the W3C Music Notation Community
+// Group under the W3C Community Final Specification Agreement
+// (FSA):
 //
-//    Copyright © 2004-2017 the Contributors to the MusicXML
-//    Specification, published by the W3C Music Notation Community
-//    Group under the W3C Community Final Specification Agreement
-//    (FSA):
+//    https://www.w3.org/community/about/agreements/final/
 //
-//       https://www.w3.org/community/about/agreements/final/
+// A human-readable summary is available:
 //
-//    A human-readable summary is available:
+//    https://www.w3.org/community/about/agreements/fsa-deed/
 //
-//       https://www.w3.org/community/about/agreements/fsa-deed/
-//-->
-//
-//<!--
 //    This DTD module describes the entity and elements used for
 //    simple XLink support. The link element serves as an outgoing
 //    XLink. The bookmark element serves as a well-defined target
@@ -32,8 +27,57 @@
 //    If a relative link is used within a document that is part of
 //    a compressed MusicXML file, the link is relative to the
 //    root folder of the zip file.
-//-->
 //
+extension MusicXML {
+
+    // <!ELEMENT bookmark EMPTY>
+    // <!ATTLIST bookmark
+    //    id    ID     #REQUIRED
+    //    name  CDATA  #IMPLIED
+    //    element  NMTOKEN #IMPLIED
+    //    position NMTOKEN #IMPLIED
+    // >
+    public struct Bookmark: Decodable, Equatable {
+        let id: String
+        let name: String?
+        let element: String?
+        let position: String?
+    }
+
+    // <!ELEMENT link EMPTY>
+    // <!ATTLIST link
+    //    %link-attributes;
+    //    name  CDATA  #IMPLIED
+    //    element  NMTOKEN #IMPLIED
+    //    position NMTOKEN #IMPLIED
+    //    %position;
+    // >
+    public struct Link: Decodable, Equatable {
+
+        // TODO: LinkAttributes
+
+        let name: String?
+
+        // > The element attribute specifies an
+        // > element type for a descendant of the next sibling element
+        // > that is not a link or bookmark.
+        let element: String
+
+        // The position attribute
+        //    specifies the position of this descendant element, where
+        //    the first position is 1. The position attribute is ignored
+        //    if the element attribute is not present. For instance, an
+        //    element value of "beam"  and a position value of "2" defines
+        //    the link or bookmark to refer to the second beam descendant
+        //    of the next sibling element that is not a link or bookmark.
+        //    This is equivalent to an XPath test of [.//beam[2]] done
+        //    in the context of the sibling element.
+        let position: String
+
+        // TODO: position: Position?
+    }
+}
+
 //<!-- Entities -->
 //
 //<!--
@@ -64,32 +108,6 @@
 //    higher resolution than the level of music-data elements.
 //    When no element and position attributes are present, the
 //    bookmark or link element refers to the next sibling element
-//    in the MusicXML file. The element attribute specifies an
-//    element type for a descendant of the next sibling element
-//    that is not a link or bookmark. The position attribute
-//    specifies the position of this descendant element, where
-//    the first position is 1. The position attribute is ignored
-//    if the element attribute is not present. For instance, an
-//    element value of "beam"  and a position value of "2" defines
-//    the link or bookmark to refer to the second beam descendant
-//    of the next sibling element that is not a link or bookmark.
-//    This is equivalent to an XPath test of [.//beam[2]] done
-//    in the context of the sibling element.
+//    in the MusicXML file.
 //-->
 //
-//<!ELEMENT link EMPTY>
-//<!ATTLIST link
-//    %link-attributes;
-//    name  CDATA  #IMPLIED
-//    element  NMTOKEN #IMPLIED
-//    position NMTOKEN #IMPLIED
-//    %position;
-//>
-//
-//<!ELEMENT bookmark EMPTY>
-//<!ATTLIST bookmark
-//    id    ID     #REQUIRED
-//    name  CDATA  #IMPLIED
-//    element  NMTOKEN #IMPLIED
-//    position NMTOKEN #IMPLIED
-//>
--- Sources/MusicXML/Model/Elements/Score.swift
+++ Sources/MusicXML/Model/Elements/Score.swift
@@ -59,7 +59,7 @@ extension MusicXML.Score {
         let movementTitle: String?
         let identification: MusicXML.Identification?
         // TODO: defaults
-        // TODO: credit *
+        let credits: [Credit]?
         let partList: PartList
     }
 }
@@ -363,6 +363,103 @@ extension MusicXML.Score.Timewise {
     }
 }
 
+extension MusicXML.Score.Header {
+
+    // > Credit elements refer to the title, composer, arranger,
+    // > lyricist, copyright, dedication, and other text, symbols,
+    // > and graphics that commonly appear on the first page of a
+    // > score. The credit-words, credit-symbol, and credit-image
+    // > elements are similar to the words, symbol, and image
+    // > elements for directions. However, since the credit is not
+    // > part of a measure, the default-x and default-y attributes
+    // > adjust the origin relative to the bottom left-hand corner
+    // > of the page. The  enclosure for credit-words and
+    // > credit-symbol is none by default.
+    // > By default, a series of credit-words and credit-symbol
+    // > elements within a single credit element follow one another
+    // > in sequence visually. Non-positional formatting attributes
+    // > are carried over from the previous element by default.
+    //
+    // <!ELEMENT credit
+    //    (credit-type*, link*, bookmark*,
+    //     (credit-image |
+    //      ((credit-words | credit-symbol),
+    //       (link*, bookmark*, (credit-words | credit-symbol))*)))>
+    // <!ATTLIST credit
+    //    page NMTOKEN #IMPLIED
+    //    %optional-unique-id;
+    // >
+    #warning("TODO: Get head around Credit definition 🤯")
+    public struct Credit: Decodable, Equatable {
+
+        // <!ELEMENT credit-words (#PCDATA)>
+        // <!ATTLIST credit-words
+        //    %text-formatting;
+        //    %optional-unique-id;
+        // >
+        public struct Words: Decodable, Equatable {
+            let text: String
+            let formatting: MusicXML.TextFormatting
+            let optionalUniqueID: String?
+        }
+
+        // > The credit-symbol element specifies a musical symbol
+        // > using a canonical SMuFL glyph name.
+        //
+        // <!ELEMENT credit-symbol (#PCDATA)>
+        // <!ATTLIST credit-symbol
+        //    %symbol-formatting;
+        //    %optional-unique-id;
+        // >
+        public struct Symbol: Decodable, Equatable {
+            let name: String
+            let symbolFormatting: MusicXML.SymbolFormatting?
+            let optionalUniqueID: String?
+        }
+
+        // <!ELEMENT credit-image EMPTY>
+        // <!ATTLIST credit-image
+        //    source CDATA #REQUIRED
+        //    type CDATA #REQUIRED
+        //    height %tenths; #IMPLIED
+        //    width %tenths; #IMPLIED
+        //    %position;
+        //    %halign;
+        //    %valign-image;
+        //    %optional-unique-id;
+        // >
+        public struct Image: Decodable, Equatable {
+            let source: String
+            let type: String
+            let height: Int?
+            let width: Int?
+            let position: MusicXML.Position?
+            let horizontalAlignment: MusicXML.HorizonalAlignment?
+            let verticalAlignment: MusicXML.VerticalAlignment?
+            let optionalUniqueID: String?
+        }
+
+        // > The credit-type element indicates the purpose behind a
+        // > credit. Multiple types of data may be combined in a single
+        // > credit, so multiple elements may be used. Standard values
+        // > include page number, title, subtitle, composer, arranger,
+        // > lyricist, and rights.
+        // <!ELEMENT credit-type (#PCDATA)>
+        let types: [String]?
+        let links: [MusicXML.Link]?
+        let bookmarks: [MusicXML.Bookmark]?
+
+        // > The page attribute for the credit element specifies the page
+        // > number where the credit should appear. This is an integer
+        // > value that starts with 1 for the first page. Its value is 1
+        // > by default. Since credits occur before the music, these page
+        // > numbers do not refer to the page numbering specified by the
+        // > print element's page-number attribute.
+        let page: Int?
+        let optionalUniqueID: String?
+    }
+}
+
 // MARK: - Decoding
 
 extension MusicXML.Score.Timewise: Decodable {
@@ -514,8 +611,8 @@ extension MusicXML.Score.Header: Decodable {
         case movementTitle = "movement-title"
         case partList = "part-list"
         case identification
+        case credits = "credit"
         // TODO: defaults
-        // TODO: credits = "credit"
     }
 }
 
@@ -555,73 +652,6 @@ extension MusicXML.Score.Header: Decodable {
 //    number NMTOKEN #IMPLIED
 //    name CDATA #IMPLIED
 //    xml:lang NMTOKEN #REQUIRED
-//>
-//
-//<!--
-//    Credit elements refer to the title, composer, arranger,
-//    lyricist, copyright, dedication, and other text, symbols,
-//    and graphics that commonly appear on the first page of a
-//    score. The credit-words, credit-symbol, and credit-image
-//    elements are similar to the words, symbol, and image
-//    elements for directions. However, since the credit is not
-//    part of a measure, the default-x and default-y attributes
-//    adjust the origin relative to the bottom left-hand corner
-//    of the page. The  enclosure for credit-words and
-//    credit-symbol is none by default.
-//    By default, a series of credit-words and credit-symbol
-//    elements within a single credit element follow one another
-//    in sequence visually. Non-positional formatting attributes
-//    are carried over from the previous element by default.
-//    The page attribute for the credit element specifies the page
-//    number where the credit should appear. This is an integer
-//    value that starts with 1 for the first page. Its value is 1
-//    by default. Since credits occur before the music, these page
-//    numbers do not refer to the page numbering specified by the
-//    print element's page-number attribute.
-//    The credit-type element indicates the purpose behind a
-//    credit. Multiple types of data may be combined in a single
-//    credit, so multiple elements may be used. Standard values
-//    include page number, title, subtitle, composer, arranger,
-//    lyricist, and rights.
-//-->
-//<!ELEMENT credit
-//    (credit-type*, link*, bookmark*,
-//     (credit-image |
-//      ((credit-words | credit-symbol),
-//       (link*, bookmark*, (credit-words | credit-symbol))*)))>
-//<!ATTLIST credit
-//    page NMTOKEN #IMPLIED
-//    %optional-unique-id;
-//>
-//
-//<!ELEMENT credit-type (#PCDATA)>
-//
-//<!ELEMENT credit-words (#PCDATA)>
-//<!ATTLIST credit-words
-//    %text-formatting;
-//    %optional-unique-id;
-//>
-//
-//<!--
-//    The credit-symbol element specifies a musical symbol
-//    using a canonical SMuFL glyph name.
-//-->
-//<!ELEMENT credit-symbol (#PCDATA)>
-//<!ATTLIST credit-symbol
-//    %symbol-formatting;
-//    %optional-unique-id;
-//>
-//
-//<!ELEMENT credit-image EMPTY>
-//<!ATTLIST credit-image
-//    source CDATA #REQUIRED
-//    type CDATA #REQUIRED
-//    height %tenths; #IMPLIED
-//    width %tenths; #IMPLIED
-//    %position;
-//    %halign;
-//    %valign-image;
-//    %optional-unique-id;
 //>
 //
 
