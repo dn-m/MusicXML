dd18c154f1bf7b5326a310bc9dcfb579e820b89d
James Bean <jbean46@gmail.com>
2019-01-25 15:50:19 -0500

    Update XMLCoder to 0.3.0 (#24)

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["MusicXML"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/MaxDesiatov/XMLCoder", from: "0.2.1")
+        .package(url: "https://github.com/MaxDesiatov/XMLCoder", from: "0.3.0")
     ],
     targets: [
         .target(
