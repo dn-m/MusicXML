2d0b1745dbfb2ecc0256ec049ac00f489c4d4636
James <jbean46@gmail.com>
2019-04-28 14:21:41 -0400

    Remove dead initializer

--- Sources/MusicXML/Model/Elements/Attributes/Attributes.swift
+++ Sources/MusicXML/Model/Elements/Attributes/Attributes.swift
@@ -44,37 +44,6 @@ public struct Attributes: Equatable {
     let transpose: [Transpose]?
     let directive: [Directive]?
     let measureStyles: [MeasureStyle]?
-
-    // MARK: - Initializers
-
-//    public init(
-//        editorial: Editorial? = nil,
-//        divisions: Divisions? = nil,
-//        keys: [Key]? = nil,
-//        time: [Time]? = nil,
-//        staves: Staves? = nil,
-//        partSymbol: PartSymbol? = nil,
-//        instruments: Instruments? = nil,
-//        clefs: [Clef]? = nil,
-//        staffDetails: [StaffDetails]? = nil,
-//        transpose: [Transpose]? = nil,
-//        directive: [Directive]? = nil,
-//        measureStyles: [MeasureStyle]? = nil
-//    )
-//    {
-//        self.editorial = editorial
-//        self.divisions = divisions
-//        self.keys = keys
-//        self.time = time
-//        self.staves = staves
-//        self.partSymbol = partSymbol
-//        self.instruments = instruments
-//        self.clefs = clefs
-//        self.staffDetails = staffDetails
-//        self.transpose = transpose
-//        self.directive = directive
-//        self.measureStyles = measureStyles
-//    }
 }
 
 // > Musical notation duration is commonly represented as
