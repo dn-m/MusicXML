25255914f92788a1fa3f4e792c1412071eb1df52
James <jbean46@gmail.com>
2019-05-14 18:57:46 -0400

    Add Simple Types through O

---
+++ Sources/MusicXML/Simple Types/OverUnder.swift
@@ -0,0 +1,16 @@
+//
+//  OverUnder.swift
+//  MusicXML
+//
+//  Created by James Bean on 5/14/19.
+//
+
+/// The over-under type is used to indicate whether the tips of curved lines such as slurs and ties
+/// are overhand (tips down) or underhand (tips up).
+public enum OverUnder: String {
+    case over
+    case under
+}
+
+extension OverUnder: Equatable { }
+extension OverUnder: Decodable { }
