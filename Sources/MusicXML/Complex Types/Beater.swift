de41d25d62c96a3b32bbbaebbd9c88b76853bd44
James <jbean46@gmail.com>
2019-05-15 18:31:09 -0400

    Add Bend and friends

---
+++ Sources/MusicXML/Complex Types/Beater.swift
@@ -0,0 +1,14 @@
+//
+//  Beater.swift
+//  MusicXML
+//
+//  Created by James Bean on 5/15/19.
+//
+
+public struct Beater {
+    public let value: BeaterValue
+    public let tip: TipDirection?
+}
+
+extension Beater: Equatable { }
+extension Beater: Decodable { }
