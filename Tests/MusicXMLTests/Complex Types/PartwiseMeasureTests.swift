fc151b3f049223a03550d13155019ffc2a1379a8
James Bean <jbean46@gmail.com>
2019-08-04 20:58:00 +0200

    Write failing partwise measure test

--- Tests/MusicXMLTests/Complex Types/PartwiseMeasureTests.swift
+++ Tests/MusicXMLTests/Complex Types/PartwiseMeasureTests.swift
@@ -51,6 +51,71 @@ class PartwiseMeasureTests: XCTestCase {
         """
         let decoded = try XMLDecoder()
             .decode(Score.Partwise.Measure.self, from: xml.data(using: .utf8)!)
-        #warning("Finish Score.Partwise.Measure attributes tests")
+        let expected = Score.Partwise.Measure(
+            number: 1,
+            text: nil,
+            implicit: nil,
+            nonControlling: nil,
+            width: nil,
+            optionalUniqueID: nil,
+            musicData: MusicData(
+                values: [
+                    .attributes(
+                        Attributes(
+                            footnote: nil,
+                            level: nil,
+                            divisions: 1,
+                            keys: [
+                                Key(
+                                    number: nil,
+                                    position: nil,
+                                    printStyle: nil,
+                                    printObject: nil,
+                                    kind: .traditional(Key.Traditional(cancel: nil, fifths: 0, mode: .major)),
+                                    keyOctave: nil
+                                )
+                            ],
+                            times: [
+                                Time(
+                                    number: nil,
+                                    symbol: .common,
+                                    separator: nil,
+                                    printStyle: nil,
+                                    hAlign: nil,
+                                    vAlign: nil,
+                                    printObject: nil,
+                                    kind: .measured(
+                                        Time.Measured(
+                                            signature: Time.Signature(beats: 4, beatType: 4),
+                                            interchangeable: nil
+                                        )
+                                    )
+                                )
+                            ],
+                            staves: nil,
+                            partSymbol: nil,
+                            instruments: nil,
+                            clefs: [
+                                Clef(
+                                    number: nil,
+                                    additional: nil,
+                                    size: nil,
+                                    afterBarline: nil,
+                                    printStyle: nil,
+                                    printObject: nil,
+                                    sign: .g,
+                                    line: 2,
+                                    clefOctaveChange: nil
+                                )
+                            ],
+                            staffDetails: nil,
+                            transpose: nil,
+                            measureStyles: nil
+                        )
+                    )
+                ]
+            )
+        )
+        XCTAssertEqual(decoded, expected)
     }
 }
