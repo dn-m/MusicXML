5c2c5a52ebcf31c4e5b4ce5bc9fa8dc4463d0740
James Bean <jbean46@gmail.com>
2019-08-05 08:15:12 +0200

    Move Header to top level

---
+++ Sources/MusicXML/Complex Types/Header.swift
@@ -0,0 +1,39 @@
+//
+//  Header.swift
+//  MusicXML
+//
+//  Created by James Bean on 8/5/19.
+//
+
+// > The score-header entity contains basic score metadata
+// > about the work and movement, score-wide defaults for
+// > layout and fonts, credits that appear on the first page,
+// > and the part list.
+//
+// <!ENTITY % score-header
+// "(work?, movement-number?, movement-title?,
+// identification?, defaults?, credit*, part-list)">
+public struct Header: Equatable {
+    let work: Work?
+    let movementNumber: String?
+    let movementTitle: String?
+    let identification: Identification?
+    let defaults: Defaults?
+    let credits: [Credit]?
+    let partList: PartList
+}
+
+extension Header: Codable {
+
+    // MARK: - Decodable
+
+    enum CodingKeys: String, CodingKey {
+        case work
+        case movementNumber = "movement-number"
+        case movementTitle = "movement-title"
+        case partList = "part-list"
+        case identification
+        case defaults
+        case credits = "credit"
+    }
+}
