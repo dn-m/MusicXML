49455b80bc947608b2fb25da3dcca25f7cbbdbe7
James Bean <jbean46@gmail.com>
2019-08-07 20:35:28 +0200

    Add not so rigorous Timewise Pitch tests

--- Tests/MusicXMLTests/LilyPondTests/Timewise/01_Pitch/Timewise_01_Pitches.swift
+++ Tests/MusicXMLTests/LilyPondTests/Timewise/01_Pitch/Timewise_01_Pitches.swift
@@ -5,4 +5,17 @@
 //  Created by James Bean on 5/28/19.
 //
 
-enum Timewise_01_Pitches { }
+import XCTest
+import MusicXML
+
+class Timewise_01_Pitches: XCTestCase {
+
+    func test_01_Pitches() throws {
+        let _ = try MusicXML(string: A_Pitches)
+        let _ = try MusicXML(string: B_Intervals)
+        let _ = try MusicXML(string: C_NoVoiceElement)
+        let _ = try MusicXML(string: D_Microtones)
+        let _ = try MusicXML(string: E_ParenthesizedAccidentals)
+        let _ = try MusicXML(string: F_ParenthesizedMicrotoneAccidentals)
+    }
+}
