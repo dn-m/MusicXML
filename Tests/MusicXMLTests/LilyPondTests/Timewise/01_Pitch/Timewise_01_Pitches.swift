25ad4c25e0bc523ad1d12001d6b8ead6c910d7dd
James Bean <jbean46@gmail.com>
2019-08-07 22:31:43 +0200

    Remove print statement

--- Tests/MusicXMLTests/LilyPondTests/Timewise/01_Pitch/Timewise_01_Pitches.swift
+++ Tests/MusicXMLTests/LilyPondTests/Timewise/01_Pitch/Timewise_01_Pitches.swift
@@ -12,8 +12,7 @@ class Timewise_01_Pitches: XCTestCase {
 
     func test_01_Pitches() throws {
         let _ = try MusicXML(string: A_Pitches)
-        let intervals = try MusicXML(string: B_Intervals)
-        dump(intervals)
+        let _ = try MusicXML(string: B_Intervals)
         let _ = try MusicXML(string: C_NoVoiceElement)
         let _ = try MusicXML(string: D_Microtones)
         let _ = try MusicXML(string: E_ParenthesizedAccidentals)
