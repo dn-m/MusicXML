d757b7642aed613819377b8b53f2859122385218
James Bean <jbean46@gmail.com>
2019-08-08 19:14:42 +0200

    Introduce LilyPond StaffDetails tests (#49)

--- Sources/MusicXML/Complex Types/GroupSymbol.swift
+++ Sources/MusicXML/Complex Types/GroupSymbol.swift
@@ -5,12 +5,20 @@
 //  Created by James Bean on 5/16/19.
 //
 
+import XMLCoder
+
 /// The group-symbol type indicates how the symbol for a group is indicated in the score.
 public struct GroupSymbol {
-    public let value: GroupSymbolValue
-    public let position: Position?
-    public let color: Color?
+    public var value: GroupSymbolValue
+    public var position: Position?
+    public var color: Color?
 }
 
 extension GroupSymbol: Equatable { }
-extension GroupSymbol: Codable { }
+extension GroupSymbol: Codable {
+    enum CodingKeys: String, CodingKey {
+        case value = ""
+        case position
+        case color
+    }
+}
