204812d71d5199dcd2127726ddd3e4c70ea8d985
James Bean <jbean46@gmail.com>
2019-08-08 21:40:16 +0200

    Introduce Rhythm tests

---
+++ Tests/MusicXMLTests/LilyPondTests/Partwise/03_Rhythm/Partwise_03_Rhythm.swift
@@ -0,0 +1,19 @@
+//
+//  Partwise_03_Rhythm.swift
+//  MusicXMLTests
+//
+//  Created by James Bean on 8/8/19.
+//
+
+import XCTest
+import MusicXML
+
+class Partwise_03_Rhythm: XCTestCase {
+
+    func test_03_Rhythm() throws {
+        let _ = try MusicXML(string: A_Durations)
+        let _ = try MusicXML(string: B_Backup)
+        let _ = try MusicXML(string: C_DivisionChange)
+        let _ = try MusicXML(string: D_DottedDurations_Factors)
+    }
+}
