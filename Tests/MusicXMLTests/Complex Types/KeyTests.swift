40ad5923c7141f7db6e54b2a0bd0a0e167e4521d
James Bean <jbean46@gmail.com>
2019-10-07 14:05:49 -0400

    Add failing Key.NonTraditional test

--- Tests/MusicXMLTests/Complex Types/KeyTests.swift
+++ Tests/MusicXMLTests/Complex Types/KeyTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 import XMLCoder
-@testable import MusicXML
+import MusicXML
 
 class KeyTests: XCTestCase {
 
@@ -21,5 +21,28 @@ class KeyTests: XCTestCase {
         let decoded = try XMLDecoder().decode(Key.self, from: xml.data(using: .utf8)!)
         let expected = Key(fifths: 0, mode: .major)
         XCTAssertEqual(decoded, expected)
+    }
+
+    func testDecodingNonTraditional() throws {
+        let xml = """
+        <key>
+          <key-step>C</key-step>
+          <key-alter>-2</key-alter>
+          <key-step>G</key-step>
+          <key-alter>2</key-alter>
+          <key-step>D</key-step>
+          <key-alter>-1</key-alter>
+          <key-step>B</key-step>
+          <key-alter>1</key-alter>
+          <key-step>F</key-step>
+          <key-alter>0</key-alter>
+          <key-octave number="1">2</key-octave>
+          <key-octave number="2">3</key-octave>
+          <key-octave number="3">4</key-octave>
+          <key-octave number="4">5</key-octave>
+          <key-octave number="5">6</key-octave>
+        </key>
+        """
+        let decoded = try XMLDecoder().decode(Key.self, from: xml.data(using: .utf8)!)
     }
 }
