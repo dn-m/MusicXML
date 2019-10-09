5e22f50b1470f98fb8c97668154269b5bf07c734
James Bean <jbean46@gmail.com>
2019-10-08 23:47:44 -0400

    Fix Key.NonTraditional model and decoding (#120)

--- Sources/MusicXML/Complex Types/KeyOctave.swift
+++ Sources/MusicXML/Complex Types/KeyOctave.swift
@@ -5,17 +5,43 @@
 //  Created by James Bean on 5/15/19.
 //
 
-/// The key-octave element specifies in which octave an element of a key signature appears. The
-/// content specifies the octave value using the same values as the display-octave element.
-public struct KeyOctave {
-    public let number: Int
-    public let cancel: Bool
+import XMLCoder
 
-    public init(number: Int, cancel: Bool) {
+/// The key-octave element specifies in which octave an element of a key signature appears.
+public struct KeyOctave {
+
+    // MARK: - Value
+
+    /// The content specifies the octave value using the same values as the display-octave element.
+    public let value: Int
+
+    // MARK: - Attributes
+
+    /// The number attribute is a positive integer that refers to the key signature element in
+    /// left-to-right order.
+    public let number: Int
+    /// If the cancel attribute is set to yes, then this number refers to an element specified by
+    /// the cancel element. It is no by default.
+    public let cancel: Bool
+}
+
+extension KeyOctave {
+
+    // MARK: - Initializers
+
+    public init(_ value: Int, number: Int, cancel: Bool = false) {
+        self.value = value
         self.number = number
         self.cancel = cancel
     }
 }
 
 extension KeyOctave: Equatable { }
-extension KeyOctave: Codable { }
+extension KeyOctave: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.value = try container.decode(Int.self, forKey: .value)
+        self.number = try container.decode(Int.self, forKey: .number)
+        self.cancel = try container.decodeIfPresent(Bool.self, forKey: .cancel) ?? false
+    }
+}
