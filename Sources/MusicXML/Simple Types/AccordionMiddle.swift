ae3c278fbd018d79d13ac244e6e248078344ee00
Sihao Lu <lsh32768@gmail.com>
2019-10-11 02:49:31 -0700

    Fix AccordionRegistration

--- Sources/MusicXML/Simple Types/AccordionMiddle.swift
+++ Sources/MusicXML/Simple Types/AccordionMiddle.swift
@@ -5,4 +5,25 @@
 //  Created by James Bean on 5/14/19.
 //
 
-#warning("TODO: Implement AccordionMiddle")
+public struct AccordionMiddle {
+    public let value: Int
+}
+
+extension AccordionMiddle: Equatable {}
+extension AccordionMiddle: Codable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(value)
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        self.value = try container.decode(Int.self)
+    }
+}
+
+extension AccordionMiddle: ExpressibleByIntegerLiteral {
+    public init(integerLiteral value: Int) {
+        self.init(value: value)
+    }
+}
---
+++ Tests/MusicXMLTests/Complex Types/AccordionRegistrationTests.swift
@@ -0,0 +1,24 @@
+//
+//  AccordionRegistrationTests.swift
+//  MusicXMLTests
+//
+//  Created by Sihao Lu on 10/12/19.
+//
+
+import XCTest
+import XMLCoder
+@testable import MusicXML
+
+class AccordionRegistrationTests: XCTestCase {
+    func testDecoding() throws {
+        let xml = """
+        <accordion-registration>
+            <accordion-high/>
+            <accordion-middle>2</accordion-middle>
+        </accordion-registration>
+        """
+        let decoded = try XMLDecoder().decode(AccordionRegistration.self, from: xml.data(using: .utf8)!)
+        let expected = AccordionRegistration(accordionHigh: Empty(), accordionMiddle: 2)
+        XCTAssertEqual(decoded, expected)
+    }
+}
