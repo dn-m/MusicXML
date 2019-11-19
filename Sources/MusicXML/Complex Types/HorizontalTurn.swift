f0be3574678c5dc7fe3cd92a785408a085c892e8
Ben Wetherfield <bwetherfield@gmail.com>
2019-11-18 23:59:10 -0800

    Fix HorizontalTurn printstyle and trillsound

--- Sources/MusicXML/Complex Types/HorizontalTurn.swift
+++ Sources/MusicXML/Complex Types/HorizontalTurn.swift
@@ -8,12 +8,12 @@
 /// The horizontal-turn type represents turn elements that are horizontal rather than vertical.
 /// These are empty elements with print-style, placement, trill-sound, and slash attributes.
 public struct HorizontalTurn {
-    public var printStyle: PrintStyle?
+    public var printStyle: PrintStyle
     public var placement: AboveBelow?
-    public var trillSound: TrillSound?
+    public var trillSound: TrillSound
     public var slash: Bool?
 
-    public init(printStyle: PrintStyle? = nil, placement: AboveBelow? = nil, trillSound: TrillSound? = nil, slash: Bool? = nil) {
+    public init(printStyle: PrintStyle = PrintStyle(), placement: AboveBelow? = nil, trillSound: TrillSound = TrillSound(), slash: Bool? = nil) {
         self.printStyle = printStyle
         self.placement = placement
         self.trillSound = trillSound
@@ -22,4 +22,23 @@ public struct HorizontalTurn {
 }
 
 extension HorizontalTurn: Equatable { }
-extension HorizontalTurn: Codable { }
+extension HorizontalTurn: Codable {
+    enum CodingKeys: String, CodingKey {
+        case placement
+        case slash
+    }
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        printStyle = try PrintStyle(from: decoder)
+        placement = try container.decodeIfPresent(AboveBelow.self, forKey: .placement)
+        trillSound = try TrillSound(from: decoder)
+        slash = try container.decodeIfPresent(Bool.self, forKey: .slash)
+    }
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try printStyle.encode(to: encoder)
+        try container.encodeIfPresent(placement, forKey: .placement)
+        try trillSound.encode(to: encoder)
+        try container.encodeIfPresent(slash, forKey: .slash)
+    }
+}
