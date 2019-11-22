8f736049669ae319b8e34c3c9b8bfc5d221ab20a
Ben Wetherfield <bwetherfield@gmail.com>
2019-11-22 12:04:33 -0800

    Fix YesNoNumber coding

--- Sources/MusicXML/Simple Types/YesNoNumber.swift
+++ Sources/MusicXML/Simple Types/YesNoNumber.swift
@@ -9,51 +9,28 @@ import XMLCoder
 
 /// The yes-no-number type is used for attributes that can be either boolean or numeric values.
 public enum YesNoNumber {
-    case yes
-    case no
+    case yesNo(YesNo)
     case number(Double)
 }
 
 extension YesNoNumber: Equatable {}
 extension YesNoNumber: Codable {
-    enum CodingKeys: String, CodingKey { case yes, no, number }
+    enum CodingKeys: CodingKey {}
+    
     public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
         switch self {
-        case .yes:
-            try container.encodeNil(forKey: .yes)
-        case .no:
-            try container.encodeNil(forKey: .no)
+        case let .yesNo(value):
+            try value.encode(to: encoder)
         case let .number(value):
-            try container.encode(value, forKey: .number)
+            try value.encode(to: encoder)
         }
     }
 
     public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        func decode <T>(_ key: CodingKeys) throws -> T where T: Codable {
-            return try container.decode(T.self, forKey: key)
-        }
-
-        if container.contains(.yes) {
-            _ = try container.decodeNil(forKey: .yes)
-            self = .yes
-        } else if container.contains(.no) {
-            _ = try container.decodeNil(forKey: .no)
-            self = .no
-        } else if container.contains(.number) {
-            self = .number(try decode(.number))
-        } else {
-            throw DecodingError.typeMismatch(
-                YesNoNumber.self,
-                DecodingError.Context(
-                    codingPath: decoder.codingPath,
-                    debugDescription: "Unrecognized choice"
-                )
-            )
+        do {
+            self = .yesNo(try YesNo(from: decoder))
+        } catch {
+            self = .number(try Double(from: decoder))
         }
     }
 }
-
-extension YesNoNumber.CodingKeys: XMLChoiceCodingKey {}
