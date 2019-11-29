1ba7a05ce0ebf3b645db57d9f5468e883a278a32
Ben Wetherfield <bwetherfield@gmail.com>
2019-11-28 19:22:01 -0800

    Remove AttributeGroup labels for YesNo and YesNoNumber

--- Sources/MusicXML/Simple Types/Internal/SimpleTypesInternalConformance.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SimpleTypesInternalConformance.swift
-//  MusicXML
-//
-//  Created by Benjamin Wetherfield on 11/18/19.
-//
-
-extension YesNo: AttributeGroup {}
-extension YesNoNumber: AttributeGroup {}
