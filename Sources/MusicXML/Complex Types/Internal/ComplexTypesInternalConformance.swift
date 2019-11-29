c9cf8892bec2c391c5b6c21c9181af17b5f05a85
Ben Wetherfield <bwetherfield@gmail.com>
2019-11-28 19:47:42 -0800

    Revert "Add Attribute Group Dynamic Node Encoding" (#223)
    * Revert "Add encoding rule to attribute groups (#218)"
    
    This reverts commit b0b5abeac6062eca74b45d4ecaba6f6bfa154ba0.
    
    * Remove AttributeGroup labels for YesNo and YesNoNumber

--- Sources/MusicXML/Complex Types/Internal/ComplexTypesInternalConformance.swift
+++ Sources/MusicXML/Complex Types/Internal/ComplexTypesInternalConformance.swift
@@ -5,70 +5,16 @@
 //  Created by Benjamin Wetherfield on 11/18/19.
 //
 
-import XMLCoder
-
-extension BendSound: AttributeGroup {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
-extension Bezier: AttributeGroup {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
-extension DashedFormatting: AttributeGroup {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
-extension Font: AttributeGroup {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
-extension PlacementPrintStyle {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
-extension Position: AttributeGroup {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
-extension Printout: AttributeGroup {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
-extension PrintStyle: AttributeGroup {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
-extension PrintStyleAlign: AttributeGroup {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
-extension PrintStyleAlignObject: AttributeGroup {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
-extension PrintStyleTrillSound: AttributeGroup {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
-extension TextDecoration: AttributeGroup {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
-extension TrillSound: AttributeGroup {
-    public static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        return .attribute
-    }
-}
+extension BendSound: AttributeGroup {}
+extension Bezier: AttributeGroup {}
+extension DashedFormatting: AttributeGroup {}
+extension Font: AttributeGroup {}
+extension PlacementPrintStyle {}
+extension Position: AttributeGroup {}
+extension Printout: AttributeGroup {}
+extension PrintStyle: AttributeGroup {}
+extension PrintStyleAlign: AttributeGroup {}
+extension PrintStyleAlignObject: AttributeGroup {}
+extension PrintStyleTrillSound: AttributeGroup {}
+extension TextDecoration: AttributeGroup {}
+extension TrillSound: AttributeGroup {}
--- Sources/MusicXML/InternalTypes/AttributeGroup.swift
+++ Sources/MusicXML/InternalTypes/AttributeGroup.swift
@@ -5,5 +5,4 @@
 //  Created by Benjamin Wetherfield on 11/18/19.
 //
 
-import XMLCoder
-internal protocol AttributeGroup: DynamicNodeEncoding {}
+internal protocol AttributeGroup {}
