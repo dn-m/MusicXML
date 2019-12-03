050e1a984f36b21df1da2db786cfc5e0de682107
Ben Wetherfield <bwetherfield@gmail.com>
2019-12-03 13:22:01 -0800

    Add Initializers to script

--- Sources/MusicXML/script.sh
+++ Sources/MusicXML/script.sh
@@ -3,3 +3,8 @@ if [[ -z $(grep 'Instance Properties' "$1") ]] ; then
     gsed -i -e '/public struct/ a\
     // MARK: - Instance Properties\n' "$1"
 fi
+
+if [[ -z $(grep 'Initializers' "$1") ]] ; then
+    gsed -i -e '/public init/ i\
+    // MARK: - Initializers\n' "$1"
+fi
