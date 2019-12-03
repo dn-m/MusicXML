21602645e4180afeb5a3a850a52b0de4f712197c
Ben Wetherfield <bwetherfield@gmail.com>
2019-12-02 19:33:39 -0800

    Add condition to Instance Properties script

--- Sources/MusicXML/script.sh
+++ Sources/MusicXML/script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-if [[ -z $(grep 'Instance Propertiea' "$1") ]]; then
-    gsed -i '/public struct/ a_// MARK: - Instance Properties\n_' "$1"
+if [[ -n $(grep 'Instance Propertiea' "$1") ]]; then
+    gsed -i '/public struct/ a\
+    // MARK: - Instance Properties\n' "$1"
 fi
