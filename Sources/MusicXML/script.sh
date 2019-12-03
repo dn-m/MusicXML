ba32cf50fbd07613cdaac31ce3c4a4edff9c969c
Ben Wetherfield <bwetherfield@gmail.com>
2019-12-03 13:59:23 -0800

    Add Codable MARKs to script

--- Sources/MusicXML/script.sh
+++ Sources/MusicXML/script.sh
@@ -7,8 +7,25 @@ fi
 if [[ -z $(grep 'Initializers' "$1") ]] ; then
     gsed -i "/public struct/,/public init(/ {
     s_.*public init.*_\
-    // MARK - Initializers\
+    // MARK - Initializers\n\
 \n\
+&_
+}" "$1"
+fi
+
+if [[ -z $(grep '// MARK - Encodable' "$1") ]] ; then
+    gsed -i "/: Codable/,/encode(to encoder/ {
+    s_.*encode(to encoder.*_\
+    // MARK - Encodable\n\
+\n\
+&_
+}" "$1"
+fi
+
+if [[ -z $(grep '// MARK - Decodable' "$1") ]] ; then
+    gsed -i "/: Codable/,/init(from decoder/ {
+    s_.*init(from decoder.*_\
+    // MARK - Decodable\n\
 \n\
 &_
 }" "$1"
