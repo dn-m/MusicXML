98f2ed2b0fb0b2c0b9d783ba38b75b8060130d5b
Ben Wetherfield <bwetherfield@gmail.com>
2019-12-03 17:38:20 -0800

    Remove test script

--- Sources/MusicXML/test.sh
+++
@@ -1,2 +0,0 @@
-groups="BendSound|Bezier|DashedFormatting|Font|PlacementPrintStyle|Position|Printout|PrintStyle|PrintStyleAlign|PrintStyleAlignObject|PrintStyleTrillSound|TextDecoration|TrillSound"
-gsed -r -n "/$groups/ p" "$1"
