55cd18e46d41a56d0d14fb63b89f5c7d554f7385
Ben Wetherfield <bwetherfield@gmail.com>
2019-12-04 16:35:19 -0800

    Amend script for DynamicNodeEncoding

--- Sources/MusicXML/Scripts/add-sourcery-tags.sh
+++ Sources/MusicXML/Scripts/add-sourcery-tags.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+struct=$(gsed -r -n '0,/public struct/ s/public struct (.*) \{/\1/ p' "$1")
+if [[ -z $(gsed -n '/DynamicNodeEncoding/ p' "$1") ]] ; then
+    if [[ -z $(gsed -n '/import XMLCoder/ p' "$1") ]] ; then
+        echo "import XMLCoder" >> "$1"
+    fi
+    echo "// sourcery:inline:$struct.DynamicNodeEncoding" >> "$1"
+    echo "// sourcery:end" >> "$1"
+fi
